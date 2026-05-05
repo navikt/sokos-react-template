17aab5efcff2a8d3af0ebd2948305b0989970942
mudasar187 <mudasar_cheema@hotmail.com>
2026-05-05 20:49:16 +0200

    chore: optimalisere prosjektet

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { resolve } from "node:path";
-import terser from "@rollup/plugin-terser";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig(({ mode }) => ({
 	base: "/mikrofrontend",
 	build: {
-		rollupOptions: {
-			input: resolve(__dirname, "src/App.tsx"),
+		rolldownOptions: {
+			input: resolve(import.meta.dirname, "src/App.tsx"),
 			preserveEntrySignatures: "exports-only",
 			external: ["react", "react-dom"],
 			output: {
@@ -41,12 +39,5 @@ export default defineConfig(({ mode }) => ({
 			}),
 		},
 	},
-	plugins: [
-		react(),
-		cssInjectedByJsPlugin(),
-		EnvironmentPlugin({
-			NODE_ENV: process.env.NODE_ENV || "development",
-		}),
-		terser(),
-	],
+	plugins: [react(), cssInjectedByJsPlugin()],
 }));
