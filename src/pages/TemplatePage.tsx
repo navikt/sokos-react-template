6ee339ff6efb4fbced688000d3aa451c38d8cff4
mudasar187 <mudasar_cheema@hotmail.com>
2026-05-06 12:11:32 +0200

    trigge deploy for å sjekke

--- src/pages/TemplatePage.tsx
+++ src/pages/TemplatePage.tsx
@@ -18,6 +18,7 @@ export default function TemplatePage() {
 		{ id: 1, name: "Ola Nordmann", profession: "Lærer" },
 		{ id: 2, name: "Kari Nordmann", profession: "Sykepleier" },
 		{ id: 3, name: "Per Hansen", profession: "Elektriker" },
+		{ id: 3, name: "Gunnar Olsen", profession: "It-konsulent" },
 	];
 
 	return (
